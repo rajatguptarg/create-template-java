cc955fb09ebe829c777454838110d028cc17df72
Rajat Gupta <rajatg@thoughtworks.com>
2015-09-28 11:28:59 +0530

    Fixed Issues

---
+++ {{cookiecutter.repo_name}}/{{ cookiecutter.class_name }}.java
@@ -0,0 +1,3 @@
+/*
+ * This file is created by {{ cookiecutter.author }}.
+ */
--- {{cookiecutter.repo_name}}/{{ cookiecutter.project_name }}.java
+++
@@ -1,9 +0,0 @@
-/*
- * This file is created by {{ cookiecutter.author }}.
- */
-
-public class {{ cookiecutter.class_name }} {
-    /*
-     * Create your variables here.
-     */
-}
